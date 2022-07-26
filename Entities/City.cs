d6163e9e6ab4ccfd74d0fa80fe2e1fc9c52ececc
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-07-26 06:38:59 -0700

    Create entities

---
+++ Entities/City.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfoAPI.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+            = new List<PointOfInterest>();
+
+        public City(string name)
+        {
+            Name = name;
+        }
+    }
+}

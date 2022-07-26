daeacf9994ed558d61089b661b10d44a550231ca
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-07-26 07:44:40 -0700

    Create City entity

--- Entities/City.cs
+++ Entities/City.cs
@@ -12,7 +12,7 @@ namespace CityInfoAPI.Entities
         [Required]
         [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
-        
+
         [MaxLength(200)]
         public string? Description { get; set; }
 
